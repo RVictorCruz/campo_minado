9d0495ab5a4ef11f0fe5a17fa5c5289ed32b6338
Raimundo Victor <raimundovictor21@outlook.com>
2023-06-03 16:10:46 -0300

    jogo
    2

---
+++ lib/screens/campo_minado.dart
@@ -0,0 +1,74 @@
+import 'package:campo_minado/components/resultado_widget.dart';
+import 'package:campo_minado/components/tabuleiro_widget.dart';
+import 'package:campo_minado/models/explosao_exception.dart';
+import 'package:campo_minado/models/tabuleiro.dart';
+import 'package:flutter/material.dart';
+
+import '../models/campo.dart';
+
+class CampoMinadoApp extends StatefulWidget {
+  const CampoMinadoApp({Key? key}) : super(key: key);
+
+  @override
+  State<CampoMinadoApp> createState() => _CampoMinadoAppState();
+}
+
+class _CampoMinadoAppState extends State<CampoMinadoApp> {
+  bool? _venceu;
+  final Tabuleiro _tabuleiro =
+      Tabuleiro(linhas: 12, colunas: 12, qtdeBombas: 3);
+
+  void _reiniciar() {
+    /*setState(() {
+      _venceu = null;
+      _tabuleiro.reiniciar();
+    });*/
+  }
+
+  void _abrir(Campo campo) {
+    if (_venceu != null) {
+      return;
+    }
+    setState(() {
+      try {
+        campo.abrir();
+        if (_tabuleiro.resolvido) {
+          _venceu = true;
+        }
+      } on ExplosaoException {
+        _venceu = false;
+        _tabuleiro.revelarBombas();
+      }
+    });
+  }
+
+  void _alternarMarcacao(Campo campo) {
+    if (_venceu != null) {
+      return;
+    }
+    setState(() {
+      campo.alternarMarcacao();
+      if (_tabuleiro.resolvido) {
+        _venceu = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: ResultadoWidget(
+            onReiniciar: _reiniciar,
+            venceu: _venceu,
+          ),
+          body: Container(
+              child: TabuleiroWidget(
+            tabuleiro: _tabuleiro,
+            onAbrir: _abrir,
+            onAlternarMarcacao: _alternarMarcacao,
+          )),
+        ));
+  }
+}
